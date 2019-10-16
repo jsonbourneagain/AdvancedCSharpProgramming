c16f63be232dc6a36befffe20f4ed1133ead41ba
Json Bourne <jsonbourneagain@outlook.com>
2019-10-16 20:28:10 +0530

    Add project files.

---
+++ SyncDelegateReview/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace SyncDelegateReview
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        //public delegate int BinaryOp(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            BinaryOp binary = new BinaryOp(Add);
+            IAsyncResult asyncResult = binary.BeginInvoke(10, 20, null, null);
+            //int ans = binary(10, 20);
+
+            int ans = binary.EndInvoke(asyncResult);
+            // These lines will not execute until
+            // the Add() method has completed.
+            Console.WriteLine("Doing more work in Main()!");
+            Console.WriteLine("10 + 20 is {0}.", ans);
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
---
+++ SynchronousDelegateReview/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronousDelegateReview
+{
+    public delegate int BinaryOp(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            BinaryOp binary = new BinaryOp(Add);
+            IAsyncResult asyncResult = binary.BeginInvoke(10, 20, null, null);
+            //int ans = binary(10, 20);
+            // while (!asyncResult.IsCompleted) you can do this too.
+
+            while (!asyncResult.AsyncWaitHandle.WaitOne(1000, true))
+            {
+                Console.WriteLine("Doing more work in Main()!");
+                //Thread.Sleep(1000);
+            }
+            // These lines will not execute until
+            // the Add() method has completed.
+            int ans = binary.EndInvoke(asyncResult);
+            Console.WriteLine("10 + 20 is {0}.", ans);
+            Console.ReadLine();
+        }
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
