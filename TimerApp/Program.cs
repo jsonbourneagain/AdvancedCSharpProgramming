05cd41a44e899ce5b1c6c03a702c6f581f61b325
Json Bourne <jsonbourneagain@outlook.com>
2019-10-17 13:28:30 +0530

    Timer app

---
+++ TimerApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"*** Working with Timer type ***");
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+            Timer t = new Timer(timeCB, "Hello from Main()", 0, 1000);
+
+            // You can also discard t as it's not used anywhere.
+            //Timer _ = new Timer(timeCB, "Hello from Main()", 0, 1000);
+
+            Console.WriteLine($"Hit enter key to terminate");
+            Console.ReadLine();
+        }
+        static void PrintTime(object state)
+        {
+            Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()} Message: {state.ToString()}");
+        }
+    }
+}
