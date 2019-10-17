4e8029e1f61b4a883278128272c309135c38652b
Json Bourne <jsonbourneagain@outlook.com>
2019-10-17 16:07:39 +0530

    Data parallelism with Parallel.ForEach()

---
+++ ThreadPool/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadPool
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} is executing PrintNumbers().");
+            Console.Write("Your numbers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i}");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThreadPool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP = System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main thread started. ThreadID: {TP.Thread.CurrentThread.ManagedThreadId}");
+            Printer p = new Printer();
+            TP.WaitCallback workItem = new TP.WaitCallback(PrintTheNumbers);
+
+            for (int i = 0; i < 10; i++)
+            {
+                TP.ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+            Console.WriteLine($"All tasks queued.");
+            Console.ReadLine();
+        }
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
