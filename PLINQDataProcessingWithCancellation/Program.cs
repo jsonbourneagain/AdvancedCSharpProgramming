58c13a7f962261c4da5ded5b3044ada74f94fb4c
Json Bourne <jsonbourneagain@outlook.com>
2019-10-17 22:29:09 +0530

    PLINQ with CancellationToken

--- PLINQDataProcessingWithCancellation/Program.cs
+++ PLINQDataProcessingWithCancellation/Program.cs
@@ -9,23 +9,46 @@ namespace PLINQDataProcessingWithCancellation
 {
     class Program
     {
+        static CancellationTokenSource cancelToken = new CancellationTokenSource();
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to start processing");
-            Console.ReadKey();
-            Console.WriteLine($"Processing");
-            Task.Factory.StartNew(() => ProcessIntData());
+            do
+            {
+                Console.WriteLine($"Processing");
+                Task.Factory.StartNew(() => ProcessIntData());
+                Console.Write("Enter Q to quit: ");
+                string answer = Console.ReadLine();
+                if (answer.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    cancelToken.Cancel();
+                    break;
+                }
+
+            } while (true);
             Console.ReadLine();
         }
         static void ProcessIntData()
         {
             int[] source = Enumerable.Range(1, 10_000_000).ToArray();
-            int[] modThreeIsZero = (from num in source
-                                    where num % 3 == 0
-                                    orderby num descending
-                                    select num
-                                    ).ToArray();
-            Console.WriteLine($"Found {modThreeIsZero.Count()} numbers that match query!");
+            //int[] modThreeIsZero = (from num in source
+            //                        where num % 3 == 0
+            //                        orderby num descending
+            //                        select num
+            //                        ).ToArray();
+            try
+            {
+                int[] modThreeIsZero = (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                                        where num % 3 == 0
+                                        orderby num descending
+                                        select num
+                                        ).ToArray();
+                Console.WriteLine($"Found {modThreeIsZero.Count()} numbers that match query!");
+
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
